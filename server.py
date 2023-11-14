0a65f90e63183dd09c061a9b771d2edc8e5589d0
gralerfics <glverfer@outlook.com>
2023-11-15 01:19:26 +0800

    feat(argparser): add simple framework.

---
+++ server.py
@@ -0,0 +1,26 @@
+import argparse
+import socket
+
+
+# ...
+
+
+def launch(address, port):
+    pass
+
+
+def cli_parser():
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument('--ip', '-i', type = str, default = 'localhost')
+    argument_parser.add_argument('--port', '-p', type = int, default = 8080)
+    return argument_parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = cli_parser()
+    
+    try:
+        launch(args.ip, args.port)
+    except KeyboardInterrupt:
+        print('Server stopping...')
+
