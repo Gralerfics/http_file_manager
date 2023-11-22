34fef6c21c48badbe61680145d3810af3c9b760f
gralerfics <glverfer@outlook.com>
2023-11-23 00:40:19 +0800

    fix(server): modify the default address to 0.0.0.0:80.

--- server.py
+++ server.py
@@ -3,12 +3,13 @@ import argparse
 
 from myhttp.server import HTTPServer
 from myhttp.log import log_print, LogLevel
+from myhttp.request import HTTPRequestHandler
 
 
 def cli_parser():
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument('--ip', '-i', type = str, default = 'localhost')
-    argument_parser.add_argument('--port', '-p', type = int, default = 8080)
+    argument_parser.add_argument('--ip', '-i', type = str, default = '0.0.0.0')
+    argument_parser.add_argument('--port', '-p', type = int, default = 80)
     return argument_parser.parse_args()
 
 
@@ -16,7 +17,7 @@ if __name__ == '__main__':
     args = cli_parser()
     
     try:
-        server = HTTPServer(args.ip, args.port)
+        server = HTTPServer(args.ip, args.port, HTTPRequestHandler)
         server.launch()
     except KeyboardInterrupt:
         log_print('Shutting down...', LogLevel.INFO)
@@ -25,5 +26,6 @@ if __name__ == '__main__':
         sys.exit(0)
     except:
         log_print(f'Unknown error', LogLevel.ERROR)
-        sys.exit(1)
+        raise
+        # sys.exit(1)
 
