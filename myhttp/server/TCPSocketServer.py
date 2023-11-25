6ad5a01ffc1696bf2fd29188ad91e97b7281bacc
gralerfics <glverfer@outlook.com>
2023-11-25 12:04:21 +0800

    refactor(server): refactor TCPSocketServer and add ConnectionHandlerClass.

--- myhttp/server/TCPSocketServer.py
+++ myhttp/server/TCPSocketServer.py
@@ -5,15 +5,36 @@ import selectors
 from ..log import log_print, LogLevel
 
 
+class BaseConnectionHandlerClass:
+    def __init__(self, connection):
+        self.connection = connection
+        self.address = connection.getpeername()
+    
+    def setup(self):
+        # log_print(f'Connection from <{self.address[0]}:{self.address[1]}>: setup()', LogLevel.INFO)
+        pass
+    
+    def handle(self):
+        # log_print(f'Connection from <{self.address[0]}:{self.address[1]}>: handle()', LogLevel.INFO)
+        pass
+    
+    def shutdown(self):
+        # log_print(f'Connection from <{self.address[0]}:{self.address[1]}>: shutdown()', LogLevel.INFO)
+        pass
+
+
 class TCPSocketServer:
     backlog_size = 10
-    select_interval = 0.05
+    select_timeout = 0.1
     
-    def __init__(self, hostname, port):
+    def __init__(self, hostname, port, ConnectionHandlerClass = BaseConnectionHandlerClass):
         self.hostname = hostname
         self.port = port
-        self.selector = selectors.DefaultSelector()
-        self.sock = None
+        self.ConnectionHandlerClass = ConnectionHandlerClass
+        
+        self.welcome_socket = None
+        self.selector = selectors.DefaultSelector()                         # IO multiplexing for sockets
+        self.connection_handlers_map = {}                                   # connection socket -> connection handler
         
         self.shutdown_signal = False
         self.is_shutdown = threading.Event()
@@ -24,28 +45,28 @@ class TCPSocketServer:
     def launch(self):
         try:
             # create welcome socket
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.sock.bind((self.hostname, self.port))
-            self.sock.listen(self.backlog_size)
+            self.welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.welcome_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.welcome_socket.bind((self.hostname, self.port))
+            self.welcome_socket.listen(self.backlog_size)
             
             # register welcome socket
-            self.selector.register(self.sock, selectors.EVENT_READ)
+            self.selector.register(self.welcome_socket, selectors.EVENT_READ)
             
             # serve
             while not self.shutdown_signal:
-                events = self.selector.select(self.select_interval)
+                events = self.selector.select(self.select_timeout) # timeout for shutdown_signal detection
                 
                 for key, mask in events:
                     connection: socket.socket = key.fileobj
                     
-                    if connection == self.sock:
+                    # TODO: 需要确认除了来新数据、连接 reset，还有什么情况会触发 welcome socket / connection socket 的变动吗？
+                    if connection == self.welcome_socket:
                         # welcome socket is triggered
-                            # TODO: 除了来新数据、连接 reset，还有什么情况会触发 welcome socket 的变动吗？假如不是新连接，会导致在 accept() 时阻塞吧。
-                        self.launch_connection()
+                        new_connection_handler = self.launch_connection()
+                        self.connection_handlers_map[connection] = new_connection_handler
                     else:
                         # connection socket is triggered
-                            # TODO: 除了来新数据、连接 reset，还有什么情况会触发 connection socket 的变动吗？
                         try:
                             self.handle_connection(connection)
                         except ConnectionError: # containing ConnectionResetError, ConnectionAbortedError, etc.
@@ -53,32 +74,32 @@ class TCPSocketServer:
         finally:
             self.shutdown_signal = False
             self.is_shutdown.set()
-            self.sock.close()
+            self.welcome_socket.close()
     
     def shutdown(self):
         self.shutdown_signal = True
         self.is_shutdown.wait()
     
     def launch_connection(self):
-        # accept and register connection socket
-        connection, address = self.sock.accept()
-        self.selector.register(connection, selectors.EVENT_READ)
-        log_print(f'Connection from <{address[0]}:{address[1]}> registered', LogLevel.INFO)
-
-    def shutdown_connection(self, connection):
-        address = connection.getpeername()
-        
-        # unregister connection socket
-            # TODO: 这么写的话, 在关闭服务器后其它 client recv(x) 会一直收到 b'', 不会有异常
-        self.selector.unregister(connection)
-        connection.shutdown(socket.SHUT_WR)
-        connection.close()
-        log_print(f'Connection from <{address[0]}:{address[1]}> unregistered', LogLevel.INFO)
-    
-    def connection_send(self, connection, data):
-        log_print(f'Data to <{connection.getpeername()[0]}:{connection.getpeername()[1]}>: {data}', 'RAW_DATA')
-        connection.send(data)
+        connection, address = self.welcome_socket.accept()                  # accept new connection socket
+        self.selector.register(connection, selectors.EVENT_READ)            # register
+        connection_handler = self.ConnectionHandlerClass(connection)        # encapsulate
+        connection_handler.setup()                                          # lifecycle: setup()
+        return connection_handler
 
     def handle_connection(self, connection):
-        pass
+        connection_handler = self.connection_handlers_map.get(connection)   # get connection handler
+        connection_handler.handle()                                         # lifecycle: handle()
+
+    def shutdown_connection(self, connection):
+        connection_handler = self.connection_handlers_map.pop(connection)   # get and pop connection handler
+        connection_handler.shutdown()                                       # lifecycle: shutdown()
+        self.selector.unregister(connection)                                # unregister
+        connection.shutdown(socket.SHUT_WR)
+        connection.close()
+        # TODO: 备注，这么写的话, 在关闭服务器后其它 client recv(x) 会一直收到 b'', 不会有异常
+    
+    # def connection_send(self, connection, data):
+    #     log_print(f'Data to <{connection.getpeername()[0]}:{connection.getpeername()[1]}>: {data}', 'RAW_DATA')
+    #     connection.send(data)
 
