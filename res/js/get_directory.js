0a819da7d08bcfd1c0dcdd84036feda571f90315
gralerfics <glverfer@outlook.com>
2023-11-25 21:52:19 +0800

    feat(viewer): add . & .. to the example file viewer.

--- res/js/get_directory.js
+++ res/js/get_directory.js
@@ -1,13 +1,13 @@
 var pathInput = document.getElementById('pathInput');
 var directoryList = document.getElementById('directoryList');
 
-list.forEach(function(item) {
+["..", "."].concat(list).forEach(function(item) {
     var linkItem = document.createElement('a');
 
     linkItem.textContent = item;
     linkItem.href = path + '/' + item;
 
-    listItem = document.createElement('li');
+    var listItem = document.createElement('li');
     listItem.appendChild(linkItem);
 
     directoryList.appendChild(listItem);
